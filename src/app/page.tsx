5d2fb6e71ec8769a85e65f77983dce5b906934dd
Aadarsh Prem <101085150+aadarshprem82@users.noreply.github.com>
2026-04-11 07:01:11 +0000

    page.tsx: Add log check for search query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+import { searchKnowledge } from "@/lib/search";
+
+
 export default function Home() {
+  // console.log(searchKnowledge("cricket"));
   return (
     <main className="h-screen flex items-center justify-center bg-black text-white">
       <h1 className="text-3xl">Portfolio Engine</h1>
